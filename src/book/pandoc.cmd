0aeabee3f7f3ba5621930a2cff833e8ae61687a3
hwthinker <hwthinker@gmail.com>
2024-12-01 05:55:38 +0700

    update readme

--- src/book/pandoc.cmd
+++ src/book/pandoc.cmd
@@ -1,13 +1,1 @@
-pandoc combined.md -o output.pdf \
---pdf-engine=xelatex \
---toc --toc-depth=4 \
--V title="ESP32 Basic Starter Kit Tutorial" \
--V author="HwThinker" \
--V date="30-11-2024" \
--V "toc-title=Daftar Isi" \
--H header.tex \
--V geometry:margin=1in \
---top-level-division=chapter \
--V sectionbreak=true \
--V lof=true
-
+pandoc combined.md -o output.pdf --pdf-engine=xelatex --metadata-file=metadata.yaml
